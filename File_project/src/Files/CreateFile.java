ac268d79e1be2bdcae106e9457dba9a1977fb653
deepthi62 <121333217+deepthi62@users.noreply.github.com>
2023-02-13 17:49:41 +0530

    file uploaded

---
+++ File_project/src/Files/CreateFile.java
@@ -0,0 +1,28 @@
+package Files;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+
+	public static void main(String[] args) {
+//		File f = new File("D://class12//test789123.text");
+//		try {
+//			if(!f.exists()) {
+//				System.out.println("file is created successfully");
+//			
+//			f.createNewFile();
+//			}
+//			System.out.println("file is already created ");
+//		} catch (IOException e) {
+//			System.out.println("file creation is failed");
+//		}
+		File f = new File("D://class12//test789123.text");
+		if(f.exists()) {
+		f.delete();
+		System.out.println("file is deleted successfullu");
+		}
+		System.out.println("file is already deleted");
+	}
+
+}
